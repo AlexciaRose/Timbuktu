0b2082030fd70ef949ab7be238217c70f93703e7
Alexcia <1rosealexcia1@gmail.com>
2023-04-16 11:06:17 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    
+    <!--Stylesheets -->
+  <?php 
+        echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">';
+        echo '<link rel="stylesheet" href="Styles/nav.css">';
+  ?>
+
+    <title>Timbuktu | Get Started</title>
+</head>
+<body>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid me-5">
+                <a class="navbar-brand" href="#"><span style="color:#FA79DF;">TIM</span>BUKTU</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse d-flex mx-5" id="navbarText">
+                <ul class="navbar-nav mb-2 mb-lg-0 w-100 justify-content-evenly mx-5">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">ABOUT US</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">CATALOGUE</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">CUSTOMERS</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">PRICING</a>
+                    </li>
+                </ul>
+               
+                </div> 
+                <span class="nav-item ms-n5">
+                <a style="color:white;" class="nav-link" href="#">REGISTER NOW</a>
+                </span>
+            </div>
+        </nav>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
