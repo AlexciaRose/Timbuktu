5d4dc6e6a2412f45684e1620bd2b74746eac1e8c
Alexcia <1rosealexcia1@gmail.com>
2023-04-17 14:34:13 -0500

    nav and product changes

--- computers.php
+++ computers.php
@@ -5,79 +5,131 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
     <!--Stylesheets -->
   <?php 
-       
         echo '<link rel="stylesheet" href="Styles/nav.css">';
-        echo '<link rel="stylesheet" href="Styles/index.css">';
+        echo '<link rel="stylesheet" href="Styles/prod.css">';
   ?>
 
-    <title>Timbuktu | Get Started</title>
+    <title>Timbuktu | Products</title>
 </head>
 <body>
-        <nav class="navbar navbar-expand-lg">
+        <!-- NavBar -->    
+        <nav class="navbar navbar-expand-lg bg-dark fixed-top">
             <div class="container-fluid me-5">
-                <a class="navbar-brand" href="#"><span style="color:#FA79DF;">TIM</span>BUKTU</a>
+                <a class="navbar-brand ms-5" href="index.php"><span style="color:#FA79DF;">TIM</span>BUKTU</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse ms-5" id="navbarText">
+                <div class="collapse navbar-collapse ms-3" id="navbarText">
                     <div class="container-fluid">
-                        <ul class="navbar-nav mb-2 mb-lg-0 w-100 justify-content-evenly px-5">
+                        <ul class="navbar-nav mb-2 mb-lg-0 w-100 ps-5">
                             <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">ABOUT US</a>
+                            <a class="nav-link" href="#">About Us</a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="catalogue.php">CATALOGUE</a>
+                            <a class="nav-link active" aria-current="page" href="catalogue.php">Catalogue</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">CUSTOMERS</a>
+                                <a class="nav-link" href="#">Customers</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">PRICING</a>
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact Us</a>
                             </li>
                         </ul>
                     </div>
                
-                        <span class="nav-item">
-                            <a style="color:white;" class="nav-link" href="register.php">REGISTER</a>
+                    <div class="d-flex align-items-center">
+                        <span class="me-4">
+                            <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#searchModal">
+                                 <i class="bi bi-search" style="color:white; font-size: 1.2rem;"></i>
+                            </a>                           
                         </span>
+                        <span>
+                        <i class="bi bi-cart me-4" style="color:white; font-size: 1.5rem;"></i>
+                        </span>
+                        <span>
+                       <a href="login.php"><i title="Log In/Create Account" class="bi bi-person-fill me-5" style="color:white; font-size: 1.5rem;"></i></a> 
+                        </span>
+                    </div>
+
                 </div> 
-                
+
+                <?php include 'modal-search.php'; ?>
+
             </div>
         </nav>
 
 
+<div class="vw-100 top-info">
     
-        <?php
+</div>
+       
+        <form>
+          <div class="mt-5 mb-5">
+            <input type="text" class="form-control" id="prod-search"placeholder="Search category">
+          </div>
+        </form>
 
-            require 'connection.php';
-            // connect to database
-            $conn = Connect();
+    <div class="row ms-5 px-5 prod">
+        <div class="col-4 col-md-2 filters">
+            Filters will go here
+        </div>
+        <div class="col-8 products">
+            <div class="row mb-5">
 
-            // query to select image path from database
-            $sql = "SELECT image_url FROM products_tbl WHERE productID = 1";
+            <?php
 
-            $result = $conn->query($sql);
+               
 
-            if ($result->num_rows > 0) {
-                // output data of each row
-                while($row = $result->fetch_assoc()) {
-                    // get image path from database
-                    $image_path = $row["image_url"];
-                    
-                    // display image
-                    echo "<img src='$image_path' alt='Product Image'>";
+                require 'connection.php';
+
+                // connect to database
+                $conn = Connect();
+                $sql = "SELECT * FROM products_tbl WHERE category = 'Computers'";
+
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        // get image path from database
+                        $prod_name = $row["name"];
+                        $prod_price = $row["price"];
+                        $image_path = $row["image_url"];
+
+                        $productcard = '<div class="card col-xl col-lg-4 me-4 mb-3" style="width: 18rem;">
+                        <img src="Images/Computers/' . $image_path . '" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <a class="" href="#" style="text-decoration:none;" data-bs-toggle="modal" data-bs-target="#productModal">
+                                <h5 class="card-title">' . $prod_name . '</h5>
+                            </a> 
+                            <p class="card-text"> <strong>$' . $prod_price . '</strong></p>
+                            <a href="#" class="btn btn-light">Add to Cart</a>
+                            <a href="#" class="btn2 btn btn-light">Buy Now</a>
+                        </div>
+                    </div>';
+
+                        echo $productcard;
+                    }
+                } else {
+                    echo "No results found.";
                 }
-            } else {
-                echo "No results found.";
-            }
 
-            // close database connection
-            $conn->close();
-        ?>
+                // close database connection
+                $conn->close();
+             ?>
+
+                
+            </div>
+        </div>
+    </div>
 
 
 
---
+++ modal-search.php
@@ -0,0 +1,83 @@
+<style>
+.modal-full {
+    min-width: 100%;
+    min-height: 100%;
+    margin: 0;
+    padding: 0;
+}
+
+.modal-full .modal-content {
+    min-height: 100%;
+    border:none;
+    background-color: rgba(0, 0, 0, 0);
+}
+
+.modal-body{
+    text-align: center;
+    color: white;
+}
+
+.modal-title{
+    font-size:40px;
+    font-weight:bold;
+}
+
+.modal{
+    background-color: rgba(0, 0, 0, 0.9);
+}
+
+.modal-header{
+    border:none;
+    padding-bottom:100px;
+    padding-right:80px;
+}
+
+.btn-close {
+    color: white;
+}
+
+.form-control{
+    max-width: 800px;
+    margin: auto;
+    background-color: transparent;
+    border-bottom: 1px solid white;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    border-radius: 0%;
+    font-size:24px;
+    color:white;
+}
+
+
+
+.form-control:focus {
+    box-shadow: none;
+    border-color: #ced4da;
+    background-color: transparent;
+    color: white;
+  }
+
+</style>
+
+
+<!-- Modal -->
+<div class="modal" id="searchModal" tabindex="-1" aria-labelledby="searchModalLabel" aria-hidden="true" data-bs-backdrop="false">
+<div class="modal-dialog modal-full">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+        <i class="bi bi-x" style="font-size:4rem;"></i>
+        </button>
+      </div>
+      <div class="modal-body">
+      <h5 class="modal-title mb-4">What are you looking for?</h5>
+      <form>
+          <div class="mb-3">
+            <input type="text" class="form-control" placeholder="Enter item or keyword">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
