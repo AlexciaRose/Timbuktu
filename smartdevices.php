bf5195e2bc9ed8dae80675f8fbadca88578addec
Ashley Wellington <amariewellington@gmail.com>
2023-04-18 12:27:41 -0500

    vamos

--- smartdevices.php
+++ smartdevices.php
@@ -5,17 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-
     <!--Stylesheets -->
   <?php 
-       
         echo '<link rel="stylesheet" href="Styles/nav.css">';
-        echo '<link rel="stylesheet" href="Styles/log.css">';
+        echo '<link rel="stylesheet" href="Styles/prod.css">';
   ?>
 
-    <title>Timbuktu | Catalogue</title>
+    <title>Timbuktu | Products</title>
 </head>
 <body>
         <!-- NavBar -->    
@@ -53,7 +52,8 @@
                             </a>                           
                         </span>
                         <span>
-                        <i class="bi bi-cart me-4" style="color:white; font-size: 1.5rem;"></i>
+                        <a href="cart.php"><i title="Check Cart" class="bi bi-cart me-4" style="color:white; font-size: 1.5rem;"></i></a>
+                    
                         </span>
                         <span>
                        <a href="login.php"><i title="Log In/Create Account" class="bi bi-person-fill me-5" style="color:white; font-size: 1.5rem;"></i></a> 
@@ -61,78 +61,97 @@
                     </div>
 
                 </div> 
-                
+
                 <?php include 'modal-search.php'; ?>
+                
+
 
             </div>
         </nav>
 
 
-<h2 class="mb-5"> <span style="font-size:25px; font-weight:lighter;">Find What You're Looking For:</span> <br> Browse Our Extensive Range <br> of Products Organized by <span style="color:#FA79DF;">Category.</span></h2>
+<div class="vw-100 top-info">
     
-<div class="container mt-5">
-    <div class="d-flex justify-content-between">
-        <div class="">
-            <div class="card mt-3 border-0 me-5" style="width: 35rem;">
-                <img src="Images/complog.png" class="card-img-top" alt="..." style="height: 300px;">
-                <div class="card-body ps-0">
-                    <a href="computers.php"><h5 class="card-title mt-3">Computers and Accessories</h5></a>
-                </div>
-            </div>
-        </div>
-        <div class="">
-            <div class="card mt-3 ms-5 border-0" style="width: 35rem;">
-                <img src="Images/homelog.jpeg" class="card-img-top" alt="..." style="height: 300px;">
-                <div class="card-body ps-0">
-                    <a href="smartdevices.php"><h5 class="card-title mt-3">Smart Home Devices</h5></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="d-flex justify-content-between mt-5">
-        <div class="mt-5">
-            <div class="card mt-3 border-0 me-5" style="width: 35rem;">
-                <img src="Images/digilog.jpg" class="card-img-top" alt="..." style="height: 300px;">
-                <div class="card-body ps-0">
-                    <a href="digitalservices.php"><h5 class="card-title mt-3">Digital Streaming Services</h5></a>
-                </div>
-            </div>
-        </div>
-        <div class="mt-5">
-            <div class="card mt-3 ms-5 border-0" style="width: 35rem;">
-                <img src="Images/artilog.png" class="card-img-top" alt="..." style="height: 300px;">
-                <div class="card-body ps-0">
-                    <a href="aisolutions.php"><h5 class="card-title mt-3">Artificial Intelligence Solutions</h5></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="d-flex justify-content-between mt-5">
-        <div class="mt-5">
-            <div class="card mt-3 border-0 me-5" style="width: 35rem;">
-                <img src="Images/socilog.jpg" class="card-img-top" alt="..." style="height: 300px;">
-                <div class="card-body ps-0">
-                    <a href=""><h5 class="card-title mt-3">Online Advertising Tools</h5></a>
-                </div>
-            </div>
-        </div>
-        <div class="mt-5">
-            <div class="card mt-3 ms-5 border-0" style="width: 35rem;">
-                <img src="Images/cloudlog.jpg" class="card-img-top" alt="..." style="height: 300px;">
-                <div class="card-body ps-0">
-                    <a href=""><h5 class="card-title mt-3">Cloud Computing Services</h5></a>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
+       
+        <form action="search.php" method="POST" autocomplete="off">
+          <div class="mt-5 mb-5">
+            <input type="text" class="form-control" id="prod-search" name="prod-search" placeholder="Search Smart Devices">
+            <input type="hidden" name="category" value="smart-home-devices">
+          </div>
+        </form>
 
-<h2 class="mb-5">More products to suit your needs <br> coming soon.</h2>
+    <div class="row ms-5 ps-5 prod">
+        <div class="col-4 col-md-2 filters">
+            
+        </div>
+        <div class="col-8 products">
+            <div class=" row mb-5">
+
+            
+
+            <?php
+
+                
+                require 'connection.php';
+              
+                // connect to database
+                $conn = Connect();
+                $sql = "SELECT * FROM products_tbl WHERE category = 'smart-home-devices'";
+
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        $prod_id = $row["productID"];
+                        $prod_name = $row["name"];
+                        $prod_price = $row["price"];
+                        $image_path = $row["image_url"];
+                        $prod_cat= $row["category"];
+                        $user_id = 301;
+
+                        
+                        $productcard = '<div class="card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 ms-4 mb-3">
+                                            <img src="Images/'. $prod_cat .'/' . $image_path . '" class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <a class="" href="#" style="text-decoration:none;">
+                                                    <h5 class="card-title">' . $prod_name . '</h5>
+                                                </a> 
+                                                <p class="card-text"> <strong>$' . $prod_price . '</strong></p>
+                                                <a href="#" class="btn btn-light">Add to Cart</a>
+                                                <a href="cart.php?user_id=<?php echo $user_id; ?>" class="btn2 btn btn-light">Buy Now</a>
+                                                </div>
+                                        </div>';
+                                        
+                        echo $productcard;
+                    }
+                } else {
+                    echo "No results found.";
+                }
+
+                // close database connection
+                $conn->close();
+             ?>
+                
+            </div>
+        </div>
+    </div>
+
+
+    
+
 
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
+
+<script>
+    function setProductId(id) {
+    document.getElementById('product_id').value = id;
+}
+</script>
 
 </body>
 </html>
